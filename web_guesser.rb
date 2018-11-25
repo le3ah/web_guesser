b73e16cfa767e53712ea4caf554f138b51b53490
Leah Miller <lkmiller.87@gmail.com>
2018-11-25 11:10:21 -0700

    Adds parameters for too high and too low guesses

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,7 +1,24 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-x = rand(100)
-get '/' do
-  "The SECRET NUMBER is #{x}"
-end
+NUMBER = rand(100)
+
+  def check_guess(num_guess)
+    if num_guess.to_i > NUMBER && num_guess.to_i - NUMBER > 5
+      "Way too high!"
+    elsif num_guess.to_i < NUMBER && NUMBER - num_guess.to_i > 5
+      "Way too low!"
+    elsif num_guess.to_i < NUMBER
+      "Too low!"
+    elsif num_guess.to_i > NUMBER
+      "Too high!"
+    elsif num_guess.to_i == NUMBER
+      "You got it right! The SECRET NUMBER is #{NUMBER}"
+    end
+  end
+
+  get '/' do
+    guess = params["guess"]
+    message = check_guess(guess)
+    erb :index, :locals => {:number => NUMBER, :message => message}
+  end
