ead711ae4a0f01cbf80d19a9ed94ddca33d093a0
Leah Miller <lkmiller.87@gmail.com>
2018-11-26 18:57:16 -0700

    Adds default values

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,8 +3,10 @@ require 'sinatra/reloader'
 
 NUMBER = rand(100)
 
-  def check_guess(guess)
-    if guess.to_i > NUMBER && guess.to_i - NUMBER > 5
+  def check_guess(guess=nil)
+    if guess == nil
+      "Make a guess!"
+    elsif guess.to_i > NUMBER && guess.to_i - NUMBER > 5
       "Way too high!"
     elsif guess.to_i < NUMBER && NUMBER - guess.to_i > 5
       "Way too low!"
@@ -18,7 +20,9 @@ NUMBER = rand(100)
   end
 
   def color_feedback(output)
-    if output == "Way too high!" || output == "Way too low!"
+    if output == "Make a guess!"
+      "#34bcbc"
+    elsif output == "Way too high!" || output == "Way too low!"
       "#ef071e"
     elsif output == "Too high!" || output == "Too low!"
       "#efc2c6"
